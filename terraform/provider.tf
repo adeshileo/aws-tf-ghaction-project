e3c47d6c2433ba17437b32f2df1f49d2fd721127
adeshile <shileosun@gmail.com>
2026-09-01 15:14:00 +0100

    added terraform file

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+
+}
+
+terraform {
+    backend "s3" {
+        bucket = "tf-resources-githuba"
+        region = "us-east-1"
+        key = "github-actions/terraform.tfstate"
+        encrypt = true
+        dynamodb_table = "tf-resources-gha-lock"
+    }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "bucket_name"{
+
+description = "This is my bucket name"
+type = string
+
+}
